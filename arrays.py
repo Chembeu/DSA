eaf936c9492ec2c94a854389da48c1c44f4c5903
chembeu <simonchembeu@gmail.com>
2026-03-22 09:03:39 +0300

    looking for the heighest and lowest value in array

--- arrays.py
+++ arrays.py
@@ -1,5 +1,11 @@
 """Arrays this are data structures which contain data of the same datatype"""
 
-numbers = [3, 4, 5, 6, 7, 8, 9]
+numbers = [3, 4, 5, 6, 7, 8, 9, 2, 1, 0]
+minVal = numbers[0]
+maxVal = numbers[0]
 for number in numbers:
-    print(number)
+    if number < minVal:
+        minVal = number
+    if number > maxVal:
+        maxVal = number
+print("Heighest number:", maxVal)

5b30b53f75b530987844571fd70a19cf519654e7
chembeu <simon.shanga11@gmail.com>
2026-03-23 21:37:06 +0300

    inserting to linked list

--- lowestlinkedlist.py
+++ lowestlinkedlist.py
@@ -14,10 +14,18 @@ def findLowest(head):
     return minVal
 
 
+def insertNode(head, data):
+    new_node = Node(data)
+    new_node.next = head
+    return new_node
+
+
 node1 = Node(4)
 node2 = Node(16)
 node3 = Node(3)
 
+
 node1.next = node2
 node2.next = node3
+node1 = insertNode(node1, 2)
 print(findLowest(node1))

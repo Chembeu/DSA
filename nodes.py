4fef14eb65df374ffa3901f7703218cb92c5eb77
chembeu <simon.shanga11@gmail.com>
2026-03-24 22:31:35 +0300

    add node at any position

--- nodes.py
+++ nodes.py
@@ -26,6 +26,16 @@ def insertatend(head, data):
     temp.next = new_end
 
 
+def insertanypos(pos, data, head):
+    np = Node(data)
+    temp = head
+    for i in range(pos - 1):
+        temp = temp.next
+    np.data = data
+    np.next = temp.next
+    temp.next = np
+
+
 node1 = Node(14)
 node2 = Node(7)
 node3 = Node(34)
@@ -34,4 +44,5 @@ node1.next = node2
 node2.next = node3
 node0 = insertNode(node1, 17)
 node4 = insertatend(node0, 22)
+node7 = insertanypos(2, 66, node0)
 print(transverse(node0))

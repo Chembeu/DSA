2383999c048582bdc6f9931444eb2078c38dfd79
chembeu <simon.shanga11@gmail.com>
2026-03-24 08:56:28 +0300

    transverse and delete

--- lowestlinkedlist.py
+++ lowestlinkedlist.py
@@ -14,10 +14,32 @@ def findLowest(head):
     return minVal
 
 
+def transverse(head):
+    currentNode = head
+    while currentNode:
+        print(currentNode.data, end=" ->")
+        currentNode = currentNode.next
+    print("Null")
+
+
 def insertNode(head, data):
     new_node = Node(data)
     new_node.next = head
     return new_node
+
+
+def delete_value(head, value):
+    if head and head.data == value:
+        return head.next
+
+    current = head
+    while current.next:
+        if current.next.data == value:
+            current.next = current.next.next
+            return head
+        current = current.next
+
+    return head
 
 
 node1 = Node(4)
@@ -27,5 +49,7 @@ node3 = Node(3)
 
 node1.next = node2
 node2.next = node3
-node1 = insertNode(node1, 2)
-print(findLowest(node1))
+node0 = insertNode(node1, 2)
+node0 = delete_value(node0, 4)
+print(transverse(node0))
+print(findLowest(node0))

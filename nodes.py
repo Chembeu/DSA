bd5f8563c43f9da78f5d136325e9e98d2bf1d1d4
chembeu <simon.shanga11@gmail.com>
2026-03-24 13:04:43 +0300

    add at the end

--- nodes.py
+++ nodes.py
@@ -18,6 +18,14 @@ def insertNode(head, data):
     return new_node
 
 
+def insertatend(head, data):
+    new_end = Node(data)
+    temp = head
+    while temp.next:
+        temp = temp.next
+    temp.next = new_end
+
+
 node1 = Node(14)
 node2 = Node(7)
 node3 = Node(34)
@@ -25,4 +33,5 @@ node3 = Node(34)
 node1.next = node2
 node2.next = node3
 node0 = insertNode(node1, 17)
+node4 = insertatend(node0, 22)
 print(transverse(node0))

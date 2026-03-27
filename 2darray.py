43b1d3f5bcf8bce0f83ea8cabdafeb08409140af
chembeu <simon.shanga11@gmail.com>
2026-03-27 09:33:49 +0300

    reverse 2d array

--- 2darray.py
+++ 2darray.py
@@ -1,10 +1,13 @@
 def trasverse_arrays(number):
     m = len(number)
     n = len(number[0])
+    flat = []
 
     for i in range(m):
         for j in range(n):
-            print(number[i][j], end=" ")
+            flat.append(number[i][j])
+    reverse_array = flat[::-1]
+    print(reverse_array)
 
 
 arrays = [[1, 2, 3], [4, 6, 8], [7, 9, 6]]

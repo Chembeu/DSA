ca908bfbe8efba0b47e9f26b3f4f06a11ed03fe3
chembeu <simon.shanga11@gmail.com>
2026-03-27 09:41:07 +0300

    remove last digit

--- 2darray.py
+++ 2darray.py
@@ -10,5 +10,14 @@ def trasverse_arrays(number):
     print(reverse_array)
 
 
+def transverse(number):
+    new_array = []
+    for i in range(len(number) - 1):
+        new_array.append(number[i])
+    print(new_array)
+
+
+value = [1, 2, 3, 4, 5]
 arrays = [[1, 2, 3], [4, 6, 8], [7, 9, 6]]
 trasverse_arrays(arrays)
+transverse(value)

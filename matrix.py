474365e732b22a62ae90a7c683f20b3c46663eb4
chembeu <simon.shanga11@gmail.com>
2026-03-28 23:16:00 +0300

    add 2d matrix

--- matrix.py
+++ matrix.py
@@ -1,6 +1,8 @@
 numbers = [[2, 4, 5], [3, 5, 6], [1, 5, 7]]
 m = len(numbers)
 n = len(numbers[0])
+total_sum = 0
 for i in range(m):
     for j in range(n):
-        print(numbers[i][j])
+        total_sum += numbers[i][j]
+print(total_sum)

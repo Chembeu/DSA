e54e805484a4e754483d5ddd6aabec47f2f51b91
chembeu <simon.shanga11@gmail.com>
2026-07-11 23:02:20 +0300

    decorators

---
+++ decorators.py
@@ -0,0 +1,12 @@
+def main_function(func):
+    def wrapper(*args, **kwargs):
+        print("Before the function call")
+        result = func(*args, **kwargs)
+        print("After the function call")
+        return result
+    return wrapper
+@main_function
+def child_function(x, y):
+    print(f"Inside the child function with arguments: {x}, {y}")
+    return x + y
+child_function(6,8)
